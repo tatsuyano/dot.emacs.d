dde69aaa8e9336fc9fdc87c733e59704734c9688
Tatsuya NOGUCHI <tatsuyano@gmail.com>
2014-12-20 23:52:51 +0900

    Setting git key bind

--- init_loader/02_git-gutter.el
+++ init_loader/02_git-gutter.el
@@ -1,11 +1,13 @@
 ;; http://qiita.com/syohex/items/a669b35fbbfcdda0cbf2
+;; http://blog.happyelements.co.jp/2012/10/emacs-2-emacsgitmagit.html
 
-;; smartrep
 (require 'smartrep)
 
-;; git-gutter
 (global-git-gutter-mode t)
 
 (smartrep-define-key
-    global-map  "C-x" '(("p" . 'git-gutter:previous-diff)
+    global-map  "M-g" '(("s" . 'magit-status)
+                        ("b" . 'vc-annotate)
+                        ("d" . 'vc-diff)
+                        ("p" . 'git-gutter:previous-diff)
                         ("n" . 'git-gutter:next-diff)))
